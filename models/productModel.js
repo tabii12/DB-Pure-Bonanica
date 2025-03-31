f823580ffa4f7251e43d1e868765e0013e9fcc45
tabii <tabibito2k512@gmail.com>
2025-03-31 19:38:48 +0700

    get all products

--- models/productModel.js
+++ models/productModel.js
@@ -1,18 +1,20 @@
-//Tạo cấu trúc schema cho dữ liệu product
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  sale : { type: Number},
-  categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'categories' },
-  //ref: 'Categories' để liên kết với collection Categories
-  img: { type: String, required: true },
-  description: { type: String},
-},{versionKey: false});
+  name: { type: String, required: true }, // Tên sản phẩm (bắt buộc)
+  price: { type: Number, required: true, min: 0 }, // Giá sản phẩm (bắt buộc, >= 0)
+  images: { type: [String], required: true }, // Danh sách hình ảnh (mảng chuỗi)
+  sub_category: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "sub_categories",
+    required: true
+  }, // Liên kết với danh mục con (bắt buộc)
+  description: { type: String }, // Mô tả sản phẩm
+  ingredients: { type: [String] }, // Thành phần (mảng chuỗi)
+  usage_instructions: { type: [String] }, // Hướng dẫn sử dụng (mảng chuỗi)
+  special: { type: [String] }, // Đặc điểm sản phẩm (mảng chuỗi)
+});
 
+const Product = mongoose.model("products", productSchema);
 
-// Tạo model từ schema trên Collection products
-const productModel = mongoose.model('products', productSchema);
-
-module.exports = productModel;
+module.exports = Product;
