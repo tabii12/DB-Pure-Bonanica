a5583ad44d1bf71eef94398d5bf424cfce0c2204
tabii <tabibito2k512@gmail.com>
2025-03-31 19:15:33 +0700

    fix

--- routes/categories.js
+++ routes/categories.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-const { getAllCategories, getCategoryById, updateCategory, deleteCategory} = require('../controllers/categoryController');
+const { getAllCategories, getCategoryById, createCategory, updateCategory, deleteCategory} = require('../controllers/categoryController');
 
 //Lấy tất cả danh mục
 router.get('/', getAllCategories);
 
 //Lấy chi tiết 1 danh mục
 router.get('/:id',getCategoryById);
+
+router.get('/create', createCategory);
 
 router.get('/update/:id', updateCategory);
 
