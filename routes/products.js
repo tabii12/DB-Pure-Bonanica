22e0e953e1162377ca99cd1b2a2b6a8dbfef4d72
tabii <tabibito2k512@gmail.com>
2025-03-31 19:44:07 +0700

    add api get all

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const { getAllProducts } = require('../controllers/productController');
+
+router.get('/', getAllProducts)
 
 module.exports = router;
